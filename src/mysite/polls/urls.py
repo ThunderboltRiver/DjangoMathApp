126c9685be16401b54a9489f38be1521c4f883c6
ThunderboltRiver <lostbleuseas@outlook.jp>
2022-07-28 17:01:45 +0900

    mysql+DjangoをDocker-compose

--- src/mysite/polls/urls.py
+++ src/mysite/polls/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import IndexView, AboutView
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='index'),
+    path("", IndexView.as_view(), name="index"),
+    path("about/", AboutView.as_view(), name="index"),
 ]
--- src/mysite/polls/views.py
+++ src/mysite/polls/views.py
@@ -1,10 +1,24 @@
 # Create your views here.
+from typing import Any, Dict
+from django.views.generic import TemplateView
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(TemplateView):
+    template_name: str = "index.html"
+
+    def get_context_data(self) -> Dict[str, Any]:
+        ctxt = super().get_context_data()
+        ctxt["username"] = ""
+        return ctxt
 
 
-def about(request):
-    return HttpResponse("ここはaboutです")
+class AboutView(TemplateView):
+    template_name: str = "about.html"
+
+    def get_context_data(self) -> Dict[str, Any]:
+        ctxt = super().get_context_data()
+        ctxt["num_services"] = 1247
+        ctxt["skills"] = ["Python", "JavaScript", "Julia"]
+        # ctxt["skills"] = []
+        return ctxt
