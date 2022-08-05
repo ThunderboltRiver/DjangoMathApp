1742d8e0fa332bf5bce82b45b676bdf684bec03e
ThunderboltRiver <lostbleuseas@outlook.jp>
2022-08-05 15:55:01 +0900

    500ハンドラの追加

--- mathhub/views.py
+++ mathhub/views.py
@@ -1,7 +1,16 @@
-from xml.dom.minidom import Document
+from .models import Document
 from django.shortcuts import render
 from django.views.generic import ListView
 
 # Create your views here.
 class Index(ListView):
     model = Document
+
+
+@requires_csrf_token
+def my_customized_server_error(request, template_name="500.html"):
+    import sys
+    from django.views import debug
+
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponseServerError(error_html)
