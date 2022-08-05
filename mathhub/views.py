13dd6efb55488cafcfdf80371017aa7f111069c4
ThunderboltRiver <lostbleuseas@outlook.jp>
2022-08-05 19:30:37 +0900

    検索機能の追加とデザイン変更

--- mathhub/views.py
+++ mathhub/views.py
@@ -3,12 +3,24 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.decorators.csrf import requires_csrf_token
 from django.http import HttpResponseServerError
+from django.db.models import Q
 
 # Create your views here.
 
 
 class Index(ListView):
+    context_object_name = "document_list"
+
     model = Document
+
+    def get_queryset(self):
+        queryset = Document.objects.order_by("-created")
+        query = self.request.GET.get("query")
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) | Q(auther__icontains=query)
+            )
+        return queryset
 
 
 @requires_csrf_token
