08093369cf0c272005dd93a642e51df5254826bf
ThunderboltRiver <lostbleuseas@outlook.jp>
2022-08-05 16:42:33 +0900

    documentのリスト表示

--- mathhub/admin.py
+++ mathhub/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+
+@admin.register(models.Document)
+class DocumentAdmin(admin.ModelAdmin):
+    pass
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-
+from mathhub import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("mathhub.urls")),
 ]
+handler500 = views.my_customized_server_error
